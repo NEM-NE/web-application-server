0e50d2ca8a35e2662692d578f610ea73ad4b0ff8
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-02-05 16:36:03 +0900

    feature: 요구사항 1 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,17 +1,18 @@
 package webserver;
 
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
-
+    private static final String[] methods = { "GET", "POST", "DELETE", "PATCH" };
+    private LinkedList<String> requests = new LinkedList<String>();
     private Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
@@ -23,20 +24,35 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+
+            // 각 요청 별로 쪼개서 관리
+            String line = br.readLine();
+            while(!line.equals("")){
+                splitRequest(line);
+                line = br.readLine();
+            }
+
+            //각 요청들 파싱 & 전송하기
+            for(String request : requests){
+                byte[] body = parseRequest(request);
+                sendResponse(dos, body);
+            }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
     }
 
+    private void sendResponse(DataOutputStream dos, byte[] body) {
+        response200Header(dos, body.length);
+        responseBody(dos, body);
+    }
+
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+//            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
         } catch (IOException e) {
@@ -51,5 +67,43 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private void splitRequest(String line) {
+        for(int i = 0; i < methods.length; i++){
+            if(line.contains(methods[i])){
+                requests.add(line);
+            }
+        }
+    }
+
+    private byte[] parseRequest(String request) {
+        log.info(request);
+        String[] tokens = request.split(" ");
+        String method = tokens[0];
+        byte[] body = new byte[1];
+
+        switch (method){
+            case "GET":
+                String fileName = tokens[1];
+                body = getFile(fileName);
+                break;
+
+            default:
+                log.error("NOT FOUND METHOD!");
+        }
+
+        return body;
+    }
+
+    private byte[] getFile(String fileName) {
+        try {
+            File file = new File("./webapp" + fileName);
+            return Files.readAllBytes(Paths.get(file.getAbsolutePath()));
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+
+        return null;
     }
 }
