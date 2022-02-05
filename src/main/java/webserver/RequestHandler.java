c67f536a12fb3803fcd6f0f855b77052d06cca5c
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-02-05 21:41:22 +0900

    feature: 요구사항 5 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -4,24 +4,32 @@ import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.google.common.collect.Maps;
+import db.DataBase;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
 import util.IOUtils;
 
+import javax.xml.crypto.Data;
+
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
     private DataOutputStream dos;
+    private BufferedReader br;
     private Socket connection;
     private String main;
     private String requestBody;
     private String method;
+    private int contentLength;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -32,27 +40,13 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            br = new BufferedReader(new InputStreamReader(in));
             dos = new DataOutputStream(out);
 
             // 각 요청 별로 쪼개서 관리
-            main = br.readLine();
-            method = main.split(" ")[0];
-            String line = main;
-            int contentLength = 0;
-            while(!line.equals("")){
-                if(line.contains("Content-Length")) {
-                    contentLength = Integer.parseInt(line.substring(16, line.length()));
-                }
-                line = br.readLine();
-            }
-            // 요청 바디 확인
-            if(method.equals("POST")){
-                requestBody = IOUtils.readData(br, contentLength);
-            }
+            parseRequest();
 
-            byte[] responseBody = parseRequest(main);
-            sendResponse(responseBody);
+            runRoute(main);
 
         } catch (IOException e) {
             log.error(e.getMessage());
@@ -66,14 +60,29 @@ public class RequestHandler extends Thread {
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
-
-            if(method.equals("POST")){
-                dos.writeBytes("HTTP/1.1 302 OK \r\n");
-                dos.writeBytes("Location: http://localhost:8080/index.html \r\n");
-            }else dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
 //            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private void response302Header(DataOutputStream dos, int lengthOfBodyContent, Map<String, String> attrMap) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 OK \r\n");
+
+            Set set = attrMap.entrySet();
+            Iterator it = set.iterator();
+            while(it.hasNext()){
+                Map.Entry<String, String> entry = (Map.Entry<String, String>) it.next();
+                dos.writeBytes(entry.getKey()+ ": " + entry.getValue() + "\r\n");
+            }
+
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+            dos.flush();
         } catch (IOException e) {
             log.error(e.getMessage());
         }
@@ -88,35 +97,23 @@ public class RequestHandler extends Thread {
         }
     }
 
-    public byte[] parseRequest(String request) {
-        log.info(request);
+    public void parseRequest() throws IOException {
+        main = br.readLine();
+        method = main.split(" ")[0];
+        contentLength = 0;
 
-        String[] tokens = request.split(" ");
-        String path = tokens[1];
-        String[] paths = path.split("/");
-        String route = parseURL(paths);
-
-        byte[] body = {10, 10, 10};
-
-        //controller
-        switch (route){
-            case "/":
-                String fileName = tokens[1];
-                body = getFile(fileName);
-                break;
-            case "/user/create":
-                Map<String, String> queryMap = HttpRequestUtils.parseQueryString(requestBody);
-                User user = new User(queryMap.get("userId"), queryMap.get("password"), queryMap.get("name"), queryMap.get("email"));
-                System.out.println(user.toString());
-                break;
-            default:
-                System.out.println("DEFAULT!!!");
-                fileName = tokens[1];
-                body = getFile(fileName);
-                break;
+        String line = main;
+        while(!line.equals("")){
+            if(line.contains("Content-Length")) {
+                contentLength = Integer.parseInt(line.substring(16, line.length()));
+            }
+            line = br.readLine();
         }
 
-        return body;
+        // 요청 바디 확인
+        if(method.equals("POST")){
+            requestBody = IOUtils.readData(br, contentLength);
+        }
     }
 
     private String parseURL(String[] urls){
@@ -137,6 +134,54 @@ public class RequestHandler extends Thread {
         }
 
         return route;
+    }
+
+    public void runRoute(String request) {
+        log.info(request);
+
+        String[] tokens = request.split(" ");
+        String path = tokens[1];
+        String[] paths = path.split("/");
+        String route = parseURL(paths);
+
+        byte[] body = { 10, 10 };
+
+        //controller
+        switch (route){
+            case "/":
+                String fileName = tokens[1];
+                body = getFile(fileName);
+                sendResponse(body);
+                break;
+            case "/user/create":
+                Map<String, String> queryMap = HttpRequestUtils.parseQueryString(requestBody);
+                User user = new User(queryMap.get("userId"), queryMap.get("password"), queryMap.get("name"), queryMap.get("email"));
+                DataBase.addUser(user);
+                Map<String, String> attrMap = Maps.newHashMap();
+                attrMap.put("Location", "http://localhost:8080/index.html");
+                response302Header(dos, 0, attrMap);
+                break;
+            case "/user/login":
+                queryMap = HttpRequestUtils.parseQueryString(requestBody);
+                user = DataBase.findUserById(queryMap.get("userId"));
+                attrMap = Maps.newHashMap();
+                if(user == null || !user.getPassword().equals(queryMap.get("password"))){
+                    attrMap.put("Location", "http://localhost:8080/user/login_failed.html");
+                    attrMap.put("Set-Cookie", "logined=false");
+                    response302Header(dos, 0, attrMap);
+                }else {
+                    attrMap.put("Location", "http://localhost:8080/index.html");
+                    attrMap.put("Set-Cookie", "logined=true");
+                    response302Header(dos, 0, attrMap);
+                }
+                break;
+            default:
+                fileName = tokens[1];
+                body = getFile(fileName);
+                sendResponse(body);
+                break;
+        }
+
     }
 
     private byte[] getFile(String fileName) {
--- src/test/java/util/RequestHandlerTest.java
+++ src/test/java/util/RequestHandlerTest.java
@@ -16,15 +16,6 @@ public class RequestHandlerTest {
 
     @Test
     public void parseRequest() {
-//        String request = "GET /index.html HTTP1.1";
-        String request = "GET /user/create?userId=camper&password=1234&name=%EC%9E%84%EC%84%B1%EB%B9%88&email=dlatqdlatq%40naver.com HTTP1.1";
-        requestHandler.parseRequest(request);
-
-
-
-        request = "GET /fonts/glyphicons-halflings-regular.woff HTTP1.1";
-
-        request = "GET /user/create?userId=camper&password=1234&name=%EC%9E%84%EC%84%B1%EB%B9%88&email=dlatqdlatq%40naver.com HTTP1.1";
     }
 
 }
